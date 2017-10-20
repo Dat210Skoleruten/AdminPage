5bf89e1c0fab631955347c33757ddcbf31775e14
Alexander Gundersen <alegun93@gmail.com>
2017-10-20 16:45:58 +0200

    Added firebase APIKEY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB7LaGHqBaQxmapkSAxnqTcW6kReHhutHc",
+    authDomain: "skoleruter.firebaseapp.com",
+    databaseURL: "https://skoleruter.firebaseio.com",
+    projectId: "skoleruter",
+    storageBucket: "skoleruter.appspot.com",
+    messagingSenderId: "621434787905"
+  }
 };
