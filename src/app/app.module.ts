d532e359cbc9bce01136835d01e913a7b3063efc
Alexander Gundersen <alegun93@gmail.com>
2017-10-21 23:30:41 +0200

    Added ng2-validation, dynamic table and add/edit kommune-form
    npm install ng2-validation --save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
@@ -27,12 +29,24 @@ import { KommuneFormComponent } from './kommune-form/kommune-form.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    CustomFormsModule,    
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, canActivate: [AuthGuard] },
       { path: 'login', component: LoginComponent },
+      {
+        path: 'kommuner/new',
+        component: KommuneFormComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'kommuner/:id',
+        component: KommuneFormComponent,
+        canActivate: [AuthGuard]
+      },
     ])
   ],
   providers: [
