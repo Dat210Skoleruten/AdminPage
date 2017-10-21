a59751d39bd6d51c5c221e3ed8d3f6587cd3b8de
Alexander Gundersen <alegun93@gmail.com>
2017-10-22 00:19:48 +0200

    Search now working, no idea why....

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   filter(query: string) {
-    console.log(query);
     this.filteredKommuner = (query) ?
       this.kommuner.filter(k => k.name.toLowerCase().includes(query.toLowerCase())) :
       this.kommuner;
