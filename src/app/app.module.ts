5a25a8f5fefe0641a98eeaf015d1b5eec82fdf86
Alexander Gundersen <alegun93@gmail.com>
2017-10-21 18:34:32 +0200

    Added auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { UserService } from './services/user/user.service';
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
 
 import { AuthService } from './services/auth/auth.service';
-import { environment } from '../environments/environment';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { AuthGuard } from './guards/auth-guard/auth-guard.service';
+import { UserService } from './services/user/user.service';
 
 @NgModule({
   declarations: [
@@ -28,13 +29,14 @@ import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
+      { path: '', component: HomeComponent, canActivate: [AuthGuard] },
       { path: 'login', component: LoginComponent },
     ])
   ],
   providers: [
     AuthService,
-    UserService
+    UserService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
